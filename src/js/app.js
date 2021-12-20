ce7e1fe17e8a37ffa54bb360ab9ee1b1d0639573
Yordan <yordan_yordanov001@abv.bg>
2021-12-20 14:43:29 +0200

    ramda unistall

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 import "../scss/app.scss";
-const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const isEven = numbersArray.filter((n) => n % 2 === 0);
+  console.log(isEven);
+
+  ul.append([isEven]);
 });
