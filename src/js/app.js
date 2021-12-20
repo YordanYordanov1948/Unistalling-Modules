f647ceea6559e05574a69a3d64e8e2ce35c185a3
Yordan <yordan_yordanov001@abv.bg>
2021-12-20 15:08:01 +0200

    solution

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,15 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  var li = document.createElement("li");
+
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = numbersArray.filter((n) => n % 2 === 0);
   console.log(isEven);
 
-  ul.append([isEven]);
+  console.log(li);
+  console.log(ul);
+
+  li.append([isEven]);
+  ul.append(li);
 });
